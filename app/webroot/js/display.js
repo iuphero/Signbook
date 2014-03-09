595103e60bc92836b8ffcaba8aad5adbe3b6dbb2
Xfight <Xfight9@gmail.com>
2014-03-09 10:44:01 +0800

    changes

--- app/webroot/js/display.js
+++ app/webroot/js/display.js
@@ -2,6 +2,14 @@ signbook.display = (function (sb){
 
     sb.init = function(){
 
+        $('#monthSelector').datetimepicker({
+            format : 'yyyy-mm',
+            startView : 3,
+            minView : 3,
+            autoclose : true,
+            language : 'zh-CN'
+        });
+
         $('.sb-table td').tooltip({
             container : 'body'
         });
